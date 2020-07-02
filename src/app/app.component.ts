d5cee52489fab825a32a3191680991c23a0e3a33
Aravindakksan <v.v.aravindakksan@gmail.com>
2020-07-03 00:42:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title:String="";
+  price:number=0;
+  period:string="";
+  freeFeatures = [
+    { "name": "Single User", "available": "yes" },
+    { "name": "5GB Storage", "available": "yes" },
+    { "name": "Unlimited Public Projects", "available": "yes" },
+    { "name": "Community Access", "available": "yes" },
+    { "name": "Unlimited Private Projects", "available": "no" },
+    { "name": "Dedicated Phone Support", "available": "no" },
+    { "name": "Free Subdomain", "available": "no" },
+    { "name": "Monthly Status Reports", "available": "no" }
+  ];
+  plusFeatures = [
+    { "name": "5 Users", "available": "yes" },
+    { "name": "50GB Storage", "available": "yes" },
+    { "name": "Unlimited Public Projects", "available": "yes" },
+    { "name": "Community Access", "available": "yes" },
+    { "name": "Unlimited Private Projects", "available": "Yes" },
+    { "name": "Dedicated Phone Support", "available": "Yes" },
+    { "name": "Free Subdomain", "available": "Yes" },
+    { "name": "Monthly Status Reports", "available": "no" }
+  ];
+  proFeatures = [
+    { "name": "Unlimited Users", "available": "yes" },
+    { "name": "150GB Storage", "available": "yes" },
+    { "name": "Unlimited Public Projects", "available": "yes" },
+    { "name": "Community Access", "available": "yes" },
+    { "name": "Unlimited Private Projects", "available": "yes" },
+    { "name": "Dedicated Phone Support", "available": "yes" },
+    { "name": "Unlimited Free Subdomain", "available": "yes" },
+    { "name": "Monthly Status Reports", "available": "yes" }
+  ];
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
